9da19f52b917208074e1c7c1ef816cd1e372ccb7
Jiajie Chen <c@jia.je>
2021-09-06 13:07:47 +0800

    Fix latency tester

--- plot_memory_latency.py
+++ plot_memory_latency.py
@@ -5,16 +5,20 @@ size_data = []
 time_data = []
 
 with open('memory_latency.csv', newline='') as f:
-	r = csv.DictReader(f)
+	r = csv.reader(f)
 	for row in r:
-		size_data.append(float(row["size"]))
-		time_data.append(float(row["time(ns)"]))
+		if row[0] == "size":
+			continue
+		if len(row) != 2:
+			continue
+		size_data.append(float(row[0]))
+		time_data.append(float(row[1]))
 
 fig, ax = plt.subplots()
 
 ax.plot(size_data, time_data)
 ax.set_xscale('log')
-ax.set_ylabel('Time')
-ax.set_xlabel('Memory Block Size')
+ax.set_ylabel('Time (ns)')
+ax.set_xlabel('Memory Block Size (B)')
 
 plt.savefig('plot_memory_latency.png')
