22bf0b494e81d4fed7199b71c58a6ab831babdd7
Jiajie Chen <c@jia.je>
2022-09-26 11:41:17 +0800

    Guard perf by arg

--- src/memory_latency.cpp
+++ src/memory_latency.cpp
@@ -19,7 +19,7 @@
 FILE *fp;
 
 // measure memory latency with pointer chasing
-void test(int size, int warmup, int iterations) {
+void test(int size, int warmup, int iterations, bool perf) {
   char **buffer = generate_random_pointer_chasing(size);
   if (!buffer) {
     // skipped
@@ -36,9 +36,12 @@ void test(int size, int warmup, int iterations) {
   // benchmark
   uint64_t before = get_time();
 #ifdef __linux__
-  uint64_t cycles_before = perf_read_cycles();
-  uint64_t llc_misses_before = perf_read_llc_misses();
-  uint64_t llc_loads_before = perf_read_llc_loads();
+  uint64_t cycles_before, llc_misses_before, llc_loads_before;
+  if (perf) {
+    cycles_before = perf_read_cycles();
+    llc_misses_before = perf_read_llc_misses();
+    llc_loads_before = perf_read_llc_loads();
+  }
 #endif
 
   for (int i = 0; i < iterations; i++) {
@@ -49,17 +52,24 @@ void test(int size, int warmup, int iterations) {
   *(volatile char *)*p;
   uint64_t after = get_time();
 #ifdef __linux__
-  uint64_t cycles_after = perf_read_cycles();
-  uint64_t llc_misses_after = perf_read_llc_misses();
-  uint64_t llc_loads_after = perf_read_llc_loads();
+  uint64_t cycles_after, llc_misses_after, llc_loads_after;
+  if (perf) {
+    cycles_after = perf_read_cycles();
+    llc_misses_after = perf_read_llc_misses();
+    llc_loads_after = perf_read_llc_loads();
+  }
 #endif
 
 #ifdef __linux__
-  fprintf(fp, "%d,%.2f,%.2f,%.2f,%.2f\n", size,
-          (double)(after - before) / iterations / 100,
-          (double)(cycles_after - cycles_before) / iterations / 100,
-          (double)(llc_misses_after - llc_misses_before) / iterations / 100,
-          (double)(llc_loads_after - llc_loads_before) / iterations / 100);
+  if (perf) {
+    fprintf(fp, "%d,%.2f,%.2f,%.2f,%.2f\n", size,
+            (double)(after - before) / iterations / 100,
+            (double)(cycles_after - cycles_before) / iterations / 100,
+            (double)(llc_misses_after - llc_misses_before) / iterations / 100,
+            (double)(llc_loads_after - llc_loads_before) / iterations / 100);
+  } else {
+    fprintf(fp, "%d,%.2f\n", size, (double)(after - before) / iterations / 100);
+  }
 #else
   fprintf(fp, "%d,%.2f\n", size, (double)(after - before) / iterations / 100);
 #endif
@@ -69,19 +79,15 @@ void test(int size, int warmup, int iterations) {
 }
 
 int main(int argc, char *argv[]) {
-#ifdef __linux__
-  setup_perf_cycles();
-  setup_perf_llc_misses();
-  setup_perf_llc_loads();
-#endif
   fp = fopen("memory_latency.csv", "w");
   assert(fp);
 
   int warmup = 50000;
   int iteration = 500000;
+  bool perf = false;
 
   int opt;
-  while ((opt = getopt(argc, argv, "w:i:")) != -1) {
+  while ((opt = getopt(argc, argv, "w:i:p")) != -1) {
     switch (opt) {
     case 'w':
       sscanf(optarg, "%d", &warmup);
@@ -89,11 +95,22 @@ int main(int argc, char *argv[]) {
     case 'i':
       sscanf(optarg, "%d", &iteration);
       break;
+    case 'p':
+      perf = true;
+      break;
     default:
-      fprintf(stderr, "Usage: %s [-w warmup] [-i iteration]\n", argv[0]);
+      fprintf(stderr, "Usage: %s [-w warmup] [-i iteration] [-p]\n", argv[0]);
       exit(EXIT_FAILURE);
     }
   }
+
+#ifdef __linux__
+  if (perf) {
+    setup_perf_cycles();
+    setup_perf_llc_misses();
+    setup_perf_llc_loads();
+  }
+#endif
 
   std::map<const char *, size_t> cache_sizes = get_cache_sizes();
   for (auto it : cache_sizes) {
@@ -105,65 +122,69 @@ int main(int argc, char *argv[]) {
   }
 
 #ifdef __linux__
-  fprintf(fp, "size,time(ns),cycles,llc miss ratio,llc load ratio\n");
+  if (perf) {
+    fprintf(fp, "size,time(ns),cycles,llc miss ratio,llc load ratio\n");
+  } else {
+    fprintf(fp, "size,time(ns)\n");
+  }
 #else
   fprintf(fp, "size,time(ns)\n");
 #endif
-  test(1024, warmup, iteration);
-  test(1024 * 2, warmup, iteration);
-  test(1024 * 4, warmup, iteration);
-  test(1024 * 16, warmup, iteration);
-  test(1024 * 24, warmup, iteration);
-  test(1024 * 32, warmup, iteration);
-  test(1024 * 28, warmup, iteration);
-  test(1024 * 36, warmup, iteration);
-  test(1024 * 40, warmup, iteration);
-  test(1024 * 44, warmup, iteration);
-  test(1024 * 48, warmup, iteration);
-  test(1024 * 64, warmup, iteration);
-  test(1024 * 80, warmup, iteration);
-  test(1024 * 96, warmup, iteration);
-  test(1024 * 112, warmup, iteration);
-  test(1024 * 128, warmup, iteration);
-  test(1024 * 144, warmup, iteration);
-  test(1024 * 160, warmup, iteration);
-  test(1024 * 176, warmup, iteration);
-  test(1024 * 192, warmup, iteration);
-  test(1024 * 208, warmup, iteration);
-  test(1024 * 216, warmup, iteration);
-  test(1024 * 224, warmup, iteration);
-  test(1024 * 232, warmup, iteration);
-  test(1024 * 240, warmup, iteration);
-  test(1024 * 256, warmup, iteration);
-  test(1024 * 288, warmup, iteration);
-  test(1024 * 320, warmup, iteration);
-  test(1024 * 352, warmup, iteration);
-  test(1024 * 384, warmup, iteration);
-  test(1024 * 416, warmup, iteration);
-  test(1024 * 448, warmup, iteration);
-  test(1024 * 480, warmup, iteration);
-  test(1024 * 512, warmup, iteration);
-  test(1024 * 768, warmup, iteration);
-  test(1024 * 1024, warmup, iteration);
-  test(1024 * 1024 * 2, warmup, iteration);
-  test(1024 * 1024 * 4, warmup, iteration);
-  test(1024 * 1024 * 8, warmup, iteration);
-  test(1024 * 1024 * 16, warmup, iteration);
-  test(1024 * 1024 * 24, warmup, iteration);
-  test(1024 * 1024 * 32, warmup, iteration);
-  test(1024 * 1024 * 40, warmup, iteration);
-  test(1024 * 1024 * 48, warmup, iteration);
-  test(1024 * 1024 * 64, warmup, iteration);
-  test(1024 * 1024 * 80, warmup, iteration);
-  test(1024 * 1024 * 96, warmup, iteration);
-  test(1024 * 1024 * 128, warmup, iteration);
-  test(1024 * 1024 * 160, warmup, iteration);
-  test(1024 * 1024 * 192, warmup, iteration);
-  test(1024 * 1024 * 224, warmup, iteration);
-  test(1024 * 1024 * 256, warmup, iteration);
-  test(1024 * 1024 * 384, warmup, iteration);
-  test(1024 * 1024 * 512, warmup, iteration);
-  test(1024 * 1024 * 1024, warmup, iteration);
+  test(1024, warmup, iteration, perf);
+  test(1024 * 2, warmup, iteration, perf);
+  test(1024 * 4, warmup, iteration, perf);
+  test(1024 * 16, warmup, iteration, perf);
+  test(1024 * 24, warmup, iteration, perf);
+  test(1024 * 32, warmup, iteration, perf);
+  test(1024 * 28, warmup, iteration, perf);
+  test(1024 * 36, warmup, iteration, perf);
+  test(1024 * 40, warmup, iteration, perf);
+  test(1024 * 44, warmup, iteration, perf);
+  test(1024 * 48, warmup, iteration, perf);
+  test(1024 * 64, warmup, iteration, perf);
+  test(1024 * 80, warmup, iteration, perf);
+  test(1024 * 96, warmup, iteration, perf);
+  test(1024 * 112, warmup, iteration, perf);
+  test(1024 * 128, warmup, iteration, perf);
+  test(1024 * 144, warmup, iteration, perf);
+  test(1024 * 160, warmup, iteration, perf);
+  test(1024 * 176, warmup, iteration, perf);
+  test(1024 * 192, warmup, iteration, perf);
+  test(1024 * 208, warmup, iteration, perf);
+  test(1024 * 216, warmup, iteration, perf);
+  test(1024 * 224, warmup, iteration, perf);
+  test(1024 * 232, warmup, iteration, perf);
+  test(1024 * 240, warmup, iteration, perf);
+  test(1024 * 256, warmup, iteration, perf);
+  test(1024 * 288, warmup, iteration, perf);
+  test(1024 * 320, warmup, iteration, perf);
+  test(1024 * 352, warmup, iteration, perf);
+  test(1024 * 384, warmup, iteration, perf);
+  test(1024 * 416, warmup, iteration, perf);
+  test(1024 * 448, warmup, iteration, perf);
+  test(1024 * 480, warmup, iteration, perf);
+  test(1024 * 512, warmup, iteration, perf);
+  test(1024 * 768, warmup, iteration, perf);
+  test(1024 * 1024, warmup, iteration, perf);
+  test(1024 * 1024 * 2, warmup, iteration, perf);
+  test(1024 * 1024 * 4, warmup, iteration, perf);
+  test(1024 * 1024 * 8, warmup, iteration, perf);
+  test(1024 * 1024 * 16, warmup, iteration, perf);
+  test(1024 * 1024 * 24, warmup, iteration, perf);
+  test(1024 * 1024 * 32, warmup, iteration, perf);
+  test(1024 * 1024 * 40, warmup, iteration, perf);
+  test(1024 * 1024 * 48, warmup, iteration, perf);
+  test(1024 * 1024 * 64, warmup, iteration, perf);
+  test(1024 * 1024 * 80, warmup, iteration, perf);
+  test(1024 * 1024 * 96, warmup, iteration, perf);
+  test(1024 * 1024 * 128, warmup, iteration, perf);
+  test(1024 * 1024 * 160, warmup, iteration, perf);
+  test(1024 * 1024 * 192, warmup, iteration, perf);
+  test(1024 * 1024 * 224, warmup, iteration, perf);
+  test(1024 * 1024 * 256, warmup, iteration, perf);
+  test(1024 * 1024 * 384, warmup, iteration, perf);
+  test(1024 * 1024 * 512, warmup, iteration, perf);
+  test(1024 * 1024 * 1024, warmup, iteration, perf);
   printf("Results are written to memory_latency.csv\n");
   return 0;
 }
