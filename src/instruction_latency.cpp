c76fbad21cd3bd6b54484cb0500626489c908706
Jiajie Chen <c@jia.je>
2023-08-22 01:44:58 +0800

    Produce csv with merged result

--- src/instruction_latency.cpp
+++ src/instruction_latency.cpp
@@ -1,4 +1,6 @@
 #include "include/utils.h"
+#include <assert.h>
+#include <set>
 #include <stdio.h>
 #include <unistd.h>
 #include <utility>
@@ -17,13 +19,25 @@ int N = 1000000;
 
 #undef INSTR_TEST
 
-#define INSTR_TEST(NAME, INST, ...) std::make_pair(#NAME, test_##NAME),
+struct InstrTest {
+  const char *name;
+  const char *inst;
+  void (*test)(int);
+};
 
-std::vector<std::pair<const char *, void (*)(int)>> tests = {
+#define INSTR_TEST(NAME, INST, ...)                                            \
+  InstrTest{.name = #NAME, .inst = #INST, .test = test_##NAME},
+
+std::vector<InstrTest> tests = {
 #include "include/instrs.h"
 };
 
 #undef INSTR_TEST
+
+struct InstrInfo {
+  std::set<double> latency;
+  double throughput;
+};
 
 int main(int argc, char *argv[]) {
   bool perf = false;
@@ -47,19 +61,70 @@ int main(int argc, char *argv[]) {
   if (perf) {
     setup_time_or_cycles();
   }
+
+  // calibrate unit cycle time
   uint64_t begin = get_time_or_cycles();
-  test_int_add(N);
+  test_unit(N);
   uint64_t unit_elapsed = get_time_or_cycles() - begin;
+
+  std::map<std::string, InstrInfo> info;
+
   for (auto it : tests) {
+    std::string name = it.name;
+    if (name == "unit") {
+      continue;
+    }
+
     begin = get_time_or_cycles();
-    it.second(N);
+    it.test(N);
     uint64_t elapsed = get_time_or_cycles() - begin;
-    std::string name = it.first;
     double cycles = (double)elapsed / unit_elapsed;
-    if (name.find("_tp") != std::string::npos) {
-      printf("%s: throughput 1/%.2lf=%.2lf instructions\n", it.first, cycles, 1.0 / cycles);
+
+    std::string base_name;
+    size_t tp_index = name.find("_tp");
+    if (tp_index != std::string::npos) {
+      base_name = name.substr(0, tp_index);
+      printf("%s: throughput 1/%.2lf=%.2lf instructions\n", it.name, cycles,
+             1.0 / cycles);
+      info[base_name].throughput = cycles;
     } else {
-      printf("%s: latency %.2lf cycles\n", it.first, cycles);
+      base_name = name;
+
+      size_t last_underscore_index = name.find_last_of("_");
+      if (last_underscore_index != std::string::npos) {
+        char *end = NULL;
+        const char *start = &name.c_str()[last_underscore_index + 1];
+        strtol(start, &end, 10);
+
+        if (start != end) {
+          // strip suffix _2 etc
+          base_name = name.substr(0, last_underscore_index);
+        }
+      }
+
+      // round to 0.01
+      cycles = (double)(long)(cycles * 100) / 100.0;
+      printf("%s: latency %.2lf cycles\n", it.name, cycles);
+      info[base_name].latency.insert(cycles);
     }
   }
+
+  FILE *fp = fopen("instruction_latency.csv", "w");
+  assert(fp);
+  fprintf(fp, "name,latency,throughput\n");
+  for (auto pair : info) {
+    std::string latency;
+    auto entry = pair.second;
+    for (auto lat : entry.latency) {
+      char buffer[32];
+      std::sprintf(buffer, "%.2lf", lat);
+      if (!latency.empty()) {
+        latency += "/";
+      }
+      latency += buffer;
+    }
+    fprintf(fp, "%s,%s,%.2lf\n", pair.first.c_str(), latency.c_str(),
+            entry.throughput);
+  }
+  printf("Result written to instruction_latency.csv\n");
 }
