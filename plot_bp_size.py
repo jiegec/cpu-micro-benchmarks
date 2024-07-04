654c804628523cf7a7e404db7338192a0257c5dd
Jiajie Chen <c@jia.je>
2024-07-04 15:53:16 +0800

    Add limit

--- plot_bp_size.py
+++ plot_bp_size.py
@@ -15,7 +15,7 @@ with open('bp_size.csv', newline='') as f:
 		min_data.append(float(row["min"]))
 		avg_data.append(float(row["avg"]))
 
-plt.imshow(np.array(avg_data).reshape((11, 17)))
+plt.imshow(np.array(avg_data).reshape((11, 17)), vmax=10)
 plt.colorbar()
 
 xticks = [1,2,4,8,16,32,64,128,256,512,1024,2048,4096,8192,16384,32768,65536]
