71edb51c289bec3f112ae076d524b110aa06c027
Jiajie Chen <c@jia.je>
2024-07-09 10:45:47 +0800

    Add branch alignment figure

--- agner/testp/TestScripts/my_phr_length.py
+++ agner/testp/TestScripts/my_phr_length.py
@@ -15,16 +15,16 @@ for dummy_branches in x_data:
         parts = list(filter(lambda s: len(s) > 0, line.strip().split(" ")))
         if len(parts) > 0:
             if not heading:
-                assert parts[5] == 'BrMisCond'
+                assert parts[5] == "BrMisCond"
                 heading = True
             else:
                 data.append(int(parts[4]))
-    avg = np.average(np.array(data)) / 2000 # 2 branches, 1000 loops
-    print(dummy_branches, f'{avg:.2f}')
+    avg = np.average(np.array(data)) / 2000  # 2 branches, 1000 loops
+    print(dummy_branches, f"{avg:.2f}")
     y_data.append(avg)
 
 plt.plot(x_data, y_data)
 plt.xlabel("Num. of Dummy Branches")
 plt.ylabel("Miss Rate")
 plt.yticks([0.25, 0.50])
-plt.savefig('my_phr_length.png')
+plt.savefig("my_phr_length.png")
