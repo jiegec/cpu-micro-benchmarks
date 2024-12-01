8fa27b51d7d483054e90a71b7564d8bb61ead9e1
Jiajie Chen <c@jia.je>
2024-12-01 19:19:53 +0800

    Run detect_uarch to compute compile definitions

---
+++ src/detect_uarch.cpp
@@ -0,0 +1,103 @@
+#include "include/uarch.h"
+#include <cassert>
+#include <stdio.h>
+
+int main() {
+  enum uarch uarch = get_uarch();
+  switch (uarch) {
+  case firestorm:
+    printf("-DAPPLE_M1\n");
+    printf("-DAPPLE_M1_FIRESTORM\n");
+    break;
+  case icestorm:
+    printf("-DAPPLE_M1\n");
+    printf("-DAPPLE_M1_ICESTORM\n");
+    break;
+  case avalanche:
+    printf("-DAPPLE_M2\n");
+    printf("-DAPPLE_M2_AVALANCHE\n");
+    break;
+  case blizzard:
+    printf("-DAPPLE_M2\n");
+    printf("-DAPPLE_M2_BLIZZARD\n");
+    break;
+  case oryon:
+    printf("-DQUALCOMM_ORYON\n");
+    break;
+  case cortex_a78:
+    printf("-DARM_CORTEX_A78\n");
+    break;
+  case cortex_a77:
+    printf("-DARM_CORTEX_A77\n");
+    break;
+  case cortex_x1:
+    printf("-DARM_CORTEX_X1\n");
+    break;
+  case neoverse_n1:
+    printf("-DARM_NEOVERSE_N1\n");
+    break;
+  case neoverse_v1:
+    printf("-DARM_NEOVERSE_V1\n");
+    break;
+  case neoverse_n2:
+    printf("-DARM_NEOVERSE_N2\n");
+    break;
+  case neoverse_v2:
+    printf("-DARM_NEOVERSE_V2\n");
+    break;
+  case tsv110:
+    printf("-DHISILICON_TSV110\n");
+    break;
+  case unknown_arm64:
+    break;
+  case golden_cove:
+    printf("-DINTEL\n");
+    printf("-DINTEL_AHYBRID\n");
+    break;
+  case gracemont:
+    printf("-DINTEL\n");
+    printf("-DINTEL_AHYBRID\n");
+    break;
+  case sunny_cove:
+    printf("-DINTEL\n");
+    printf("-DINTEL_ICELAKE_SERVER\n");
+    break;
+  case skylake:
+    printf("-DINTEL\n");
+    printf("-DINTEL_SKYLAKE_SERVER\n");
+    break;
+  case broadwell:
+    printf("-DINTEL\n");
+    printf("-DINTEL_BROADWELL\n");
+    break;
+  case zen1:
+    printf("-DAMD\n");
+    printf("-DAMD_ZEN1\n");
+    break;
+  case zen2:
+    printf("-DAMD\n");
+    printf("-DAMD_ZEN2\n");
+    break;
+  case zen3:
+    printf("-DAMD\n");
+    printf("-DAMD_ZEN3\n");
+    break;
+  case zen4:
+    printf("-DAMD\n");
+    printf("-DAMD_ZEN4\n");
+    break;
+  case zen5:
+    printf("-DAMD\n");
+    printf("-DAMD_ZEN5\n");
+    break;
+  case unknown_amd64:
+    break;
+  case la464:
+    printf("-DLA464\n");
+  case unknown_loongarch64:
+    break;
+  default:
+    assert(false);
+  }
+  return 0;
+}
